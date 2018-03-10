dbacfcdc18dab414b2c5e8a735cb83ec7723cd50
Alex Lee <al8876@gmail.com>
2018-03-10 15:47:08 -0800

    Created components to App.js for Home page

--- frontend/App.js
+++ frontend/App.js
@@ -14,143 +14,81 @@ import {
 import Exponent, { Constants, ImagePicker, registerRootComponent, LinearGradient } from 'expo';
 
 export default class App extends React.Component {
-  state = {
-    image: null,
-    uploading: false,
-  };
-
-  render() {
-    let { image } = this.state;
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-      {/*UPLOAD OPTION */}
-        {/* <Text
-          style={{
-            fontSize: 20,
-            marginBottom: 20,
-            textAlign: 'center',
-            marginHorizontal: 15,
-          }}>
-          Example: Upload ImagePicker result
-        </Text> */}
-
-        {/* <Button
-          onPress={this._pickImage}
-          title="Pick an image from camera roll"
-        />
-
-        <Button onPress={this._takePhoto} title="Take a photo" /> */}
-
-      <View style={styles.container}>
-        <LinearGradient colors={ ['#5161B9', '#9C69CC'] } style={{ position: 'absolute', height:900, width:400 }}/>
-        <TouchableOpacity onPress={this._takePhoto} style={{ zIndex: 100 }}>
-          <Text style={{ fontSize:20, color:'white' }}>TAP TO BEGIN</Text>
-          <Image style={{ width: 150, height: 150 }} source={{uri: 'https://78.media.tumblr.com/48a0d13c52b402e976bc5d4416552671/tumblr_onew3c4x8a1vxu8n6o1_500.gif'}}/>    
-        </TouchableOpacity>
-      </View>
-
-        {this._maybeRenderImage()}
-        {this._maybeRenderUploadingOverlay()}
-
-        <StatusBar barStyle="default" />
-      </View>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      uploading: false,
+      screen: 1,
+    };
+  }
+  
+  // ------------------------------------------------------
+  // Pass this to props to change the state and advance step
+  // ------------------------------------------------------
+  setScreen(screen) {
+    this.setState({screen: screen})
   }
 
-  _maybeRenderUploadingOverlay = () => {
-    if (this.state.uploading) {
+  // ---------------------
+  // render state: should only have components - TODO: Dry up code and take out logic
+  // ---------------------
+  render() {
+    let { image } = this.state;
+    
+    // ------------------------------------------------------
+    // STEP 1: HOME SCREEN - Take picture using native camera
+    // ------------------------------------------------------
+    if (this.state.screen === 1) {
+      return (<HomeScreen setScreen={this.setScreen.bind(this)} nextScreen={this.state.screen}/>)
+
+    // ------------------------------------------------------
+    // STEP 2: ANALYZE SCREEN - Take picture using native camera
+    // ------------------------------------------------------
+    } else if (this.state.screen === 2) {
       return (
-        <View
-          style={[
-            StyleSheet.absoluteFill,
-            {
-              backgroundColor: 'rgba(0,0,0,0.4)',
-              alignItems: 'center',
-              justifyContent: 'center',
-            },
-          ]}>
-          <ActivityIndicator color="#fff" animating size="large" />
+        <View style={styles.container}>
+          <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
+          {/* {this._maybeRenderImage()} */}
         </View>
       );
-    }
-  };
-
-  _maybeRenderImage = () => {
-    let { image } = this.state;
-    if (!image) {
-      return;
-    }
-
-    return (
-      <View
-        style={{
-          marginTop: 30,
-          width: 250,
-          elevation: 2,
-          shadowColor: 'rgba(0,0,0,1)',
-          shadowOpacity: 0.2,
-          shadowOffset: { width: 4, height: 4 },
-          shadowRadius: 5,
-        }}>
-        <View
-          style={{
-            borderRadius: 50,
-            overflow: 'hidden',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Image source={{ uri: image }} style={{ width: 150, height: 150, borderRadius: 100 }} />
+    // ------------------------------------------------------
+    // STEP 3: SPOTIFY SCREEN - After picture was taken
+    // ------------------------------------------------------
+    } else if (this.state.screen === 3) {
+      return (
+        <View style={styles.container}>
+          <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
+          <Image style={{ width: 150, height: 150 }} source={{ uri: 'https://media.giphy.com/media/UNBbBnOLA69YA/giphy.gif' }} />
         </View>
+      )
+    }
+  }
+}
 
-        <Text
-          onPress={this._copyToClipboard}
-          onLongPress={this._share}
-          style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
-          {image}
-        </Text>
-      </View>
-    );
-  };
 
-  _share = () => {
-    Share.share({
-      message: this.state.image,
-      title: 'Check out this photo',
-      url: this.state.image,
-    });
-  };
-
-  _copyToClipboard = () => {
-    Clipboard.setString(this.state.image);
-    alert('Copied image URL to clipboard');
-  };
+// ------------------------------------------------------
+// HomeScreen Component: STEP 1
+// ------------------------------------------------------
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {screen: this.props.screen};
+  }
 
   _takePhoto = async () => {
     let pickerResult = await ImagePicker.launchCameraAsync({
       allowsEditing: false,
       aspect: [4, 3],
     });
-
     this._handleImagePicked(pickerResult);
-  };
-
-  _pickImage = async () => {
-    let pickerResult = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    this._handleImagePicked(pickerResult);
+    this.setState({ screen: 2 })
   };
 
   _handleImagePicked = async pickerResult => {
     let uploadResponse, uploadResult;
-
     try {
       this.setState({ uploading: true });
-
       if (!pickerResult.cancelled) {
         uploadResponse = await uploadImageAsync(pickerResult.uri);
         uploadResult = await uploadResponse.json();
@@ -165,32 +103,96 @@ export default class App extends React.Component {
       this.setState({ uploading: false });
     }
   };
+
+  // ------------------------------------------------------
+  // Called after the component was rendered and it was attached to the DOM.
+  // This is a good place to make AJAX requests or setTimeout.
+  // ------------------------------------------------------
+  componentDidMount() {
+    setTimeout(() => {
+      console.log('inside setTimeout 2 seconds');
+      this.props.setScreen(3);
+    }, 2000);
+  }
+
+  render() {
+    return( 
+    <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container}>
+        <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
+        <View onPress={this._takePhoto} style={{ zIndex: 100 }}>
+          <Text style={{ fontSize: 20, color: 'white' }}>TAP TO BEGIN</Text>
+          <Image style={{ width: 150, height: 150 }} source={{ uri: 'https://78.media.tumblr.com/48a0d13c52b402e976bc5d4416552671/tumblr_onew3c4x8a1vxu8n6o1_500.gif' }} />
+        </View>
+      </View>
+    </View >
+    );  
+  }
 }
 
-async function uploadImageAsync(uri) {
-  let apiUrl = 'https://file-upload-example-backend-dkhqoilqqn.now.sh/upload';
 
-  let uriParts = uri.split('.');
-  let fileType = uriParts[uriParts.length - 1];
+// TODO: Create components and put methods in accordingly
 
-  let formData = new FormData();
-  formData.append('photo', {
-    uri,
-    name: `photo.${fileType}`,
-    type: `image/${fileType}`,
+_maybeRenderImage = () => {
+  let { image } = this.state;
+  if (!image) {
+    // ------------------------------------------------------
+    return;
+  }
+  // ------------------------------------------------------
+  return (
+    <View onPress={this._spotifyShow}
+      style={{
+        marginTop: 30,
+        width: 250,
+        elevation: 2,
+        shadowColor: 'rgba(0,0,0,1)',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 4, height: 4 },
+        shadowRadius: 5,
+      }}>
+      <View
+        style={{
+          borderRadius: 50,
+          overflow: 'hidden',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Image source={{ uri: image }} style={{ width: 150, height: 150, borderRadius: 100 }} />
+      </View>
+      {/* URL LINK */}
+      {/* <Text
+        onPress={this._copyToClipboard}
+        onLongPress={this._share}
+        style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
+        {image}
+      </Text> */}
+      <Text style={{ color: 'white', fontSize: 20, paddingVertical: 30, paddingHorizontal: 30, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>  ANALYZING MOOD</Text>
+    </View>
+  );
+};
+_spotifyShow = () => {
+  this.setState({ screen: 3 })
+}
+_share = () => {
+  Share.share({
+    message: this.state.image,
+    title: 'Check out this photo',
+    url: this.state.image,
   });
+};
+_copyToClipboard = () => {
+  Clipboard.setString(this.state.image);
+  alert('Copied image URL to clipboard');
+};
 
-  let options = {
-    method: 'POST',
-    body: formData,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'multipart/form-data',
-    },
-  };
-
-  return fetch(apiUrl, options);
-}
+_pickImage = async () => {
+  let pickerResult = await ImagePicker.launchImageLibraryAsync({
+    allowsEditing: true,
+    aspect: [4, 3],
+  });
+  this._handleImagePicked(pickerResult);
+};
 
 const styles = StyleSheet.create({
   container: {
---
+++ frontend/components/analyze.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Analyze extends Component {
+  render() {
+
+    // RENDER SCREEN FOR ANALYZE
+    
+
+  }
+}
+
+export default Analyze;
