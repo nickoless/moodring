45642bfb388806966db4a70fbff37ccc02e7c1ff
Alex Lee <al8876@gmail.com>
2018-03-16 18:13:38 -0700

    logo renders to login page

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Button, Linking, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import qs from 'qs';
 import querystring from 'querystring';
-import Logo from '../assets/logo.png'
+// import Logo from '../assets/logo.png'
 
 export default class Login extends React.Component {
   state = {
@@ -23,8 +23,8 @@ export default class Login extends React.Component {
       <View style={styles.container}>
 
         <LinearGradient colors={this.props.backgroundColor} style={{ position: 'absolute', height: 900, width: 400 }} />
-        
-        {/* <Image style={{ width: 150, height: 150 }} source={{ uri: Logo }}/> */}
+        <Image style={{ width: 150, height: 100 }}source={require('../assets/logo.png')} />
+        {/* <Image style={{ width: 150, height: 150 }} source={{Logo}}/> */}
         
         <TouchableOpacity onPress={this._openWebBrowserAsync} style={{bottom: 0, borderWidth: 2, backgroundColor: '#2FD465', padding: 10, borderRadius: 100, borderColor: 'transparent'}}>
           <Text style={{color: 'white'}}>LOGIN WITH SPOTIFY</Text>
