6a089c4d280a2bff57da155e945942654aa3d64e
Alex Lee <al8876@gmail.com>
2018-03-18 14:52:53 -0700

    added Timeout component

--- frontend/components/Timeout.js
+++ frontend/components/Timeout.js
@@ -29,7 +29,7 @@ export default class Timeout extends React.Component {
     return (
       <View style={styles.container}>
         <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
-        <Image style={{ width: 250, height: 100 }} source={{ uri: 'https://vignette.wikia.nocookie.net/hellokitty/images/7/77/Sanrio_Characters_Gudetama_Image004.png/revision/latest?cb=20170403223437'}}/>
+        <Image style={{ width: 250, height: 200 }} source={{ uri: 'http://78.media.tumblr.com/d6bee4d844d4bb58dfe88ac089689f81/tumblr_nv6igeY9I71uzh6soo1_500.png'}}/>
         <Text style={{color: 'white', fontSize: 15, textAlign: 'center' }}>Uh Oh! Looks like your connection timed out! {"\n"}{"\n"} Try clearing your phones cache or restarting your phone!</Text>        
         <TouchableOpacity onPress={this._returnHome} style={{paddingTop: 50}}>
           <Text style={{color: 'white', fontSize: 20, borderWidth: 2, borderColor: 'white', paddingHorizontal: 20, paddingVertical: 10 }}>TAP HERE TO GO BACK</Text>
