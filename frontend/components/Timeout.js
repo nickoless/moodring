cf05b2a9d758ac6312e825b210b2f51afd3167b1
Alex Lee <al8876@gmail.com>
2018-03-18 16:35:59 -0700

    images for ERROR and TIMEOUT components are locally sourced

--- frontend/components/Timeout.js
+++ frontend/components/Timeout.js
@@ -13,6 +13,7 @@ import {
   WebView,
 } from 'react-native';
 import Exponent, { Constants, registerRootComponent, LinearGradient } from 'expo';
+import TimeOut from '../assets/timeout.png'
 
 
 export default class Timeout extends React.Component {
@@ -29,7 +30,7 @@ export default class Timeout extends React.Component {
     return (
       <View style={styles.container}>
         <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
-        <Image style={{ width: 250, height: 200 }} source={{ uri: 'http://78.media.tumblr.com/d6bee4d844d4bb58dfe88ac089689f81/tumblr_nv6igeY9I71uzh6soo1_500.png'}}/>
+        <Image style={{ width: 280, height: 130 }} source={TimeOut}/>
         <Text style={{color: 'white', fontSize: 15, textAlign: 'center' }}>Uh Oh! Looks like your connection timed out! {"\n"}{"\n"} Try clearing your phones cache or restarting your phone!</Text>        
         <TouchableOpacity onPress={this._returnHome} style={{paddingTop: 50}}>
           <Text style={{color: 'white', fontSize: 20, borderWidth: 2, borderColor: 'white', paddingHorizontal: 20, paddingVertical: 10 }}>TAP HERE TO GO BACK</Text>
