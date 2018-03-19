0b83502e746ceae2b9d1a361a00d9fadd3c2e64d
hvdson <hvdsonmusic@gmail.com>
2018-03-18 18:02:21 -0700

    backup for homescreen before styling

--- frontend/components/HomeScreenBackup.js
+++ frontend/components/HomeScreenBackup.js
@@ -265,40 +265,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  }, triangleCorner: {
-    position: 'absolute',
-    top: 105,
-    left: 0,
-    width: 300,
-    height: 100,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderRightWidth: 50,
-    borderTopWidth: 80,
-    borderRightColor: 'transparent',
-    borderTopColor: 'gray'
-  }, triangleCorner1: {
-    position: 'absolute',
-    top: 100,
-    left: 0,
-    width: 130,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderRightWidth: 50,
-    borderTopWidth: 90,
-    borderRightColor: 'transparent',
-    borderTopColor: 'green'
-  }, triangleCornerLayer: {
-    position: 'absolute',
-    top: 107,
-    left: 0,
-    width: 297,
-    height: 100,
-    backgroundColor: 'transparent',
-    borderStyle: 'solid',
-    borderRightWidth: 47,
-    borderTopWidth: 75,
-    borderRightColor: 'transparent',
-    borderTopColor: 'white'
-  }
+  },
 });
