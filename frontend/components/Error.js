929863cc1e2c98853630c3681fcc7cc2518265f7
Alex Lee <al8876@gmail.com>
2018-03-18 18:37:41 -0700

    ERROR and TIMEOUT Component added animation - FadeIn

--- frontend/components/Error.js
+++ frontend/components/Error.js
@@ -14,7 +14,8 @@ import {
   WebView,
 } from 'react-native';
 import Exponent, { Constants, registerRootComponent, LinearGradient } from 'expo';
-import ErrorImage from '../assets/error.png'
+import * as Animatable from 'react-native-animatable';
+import ErrorImage from '../assets/error.png';
 
 
 export default class Error extends React.Component {
@@ -38,11 +39,13 @@ export default class Error extends React.Component {
     return (
       <View style={styles.container}>
         <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
-        <Image style={{ width: 250, height: 100 }} source={ErrorImage}/>
-        <Text style={{color: 'white', fontSize: 15 }}>Uh Oh! Looks like this picture doesn't work!</Text>
-        <TouchableOpacity onPress={this._returnHome} style={{paddingTop: 50}}>
-          <Text style={{color: 'white', fontSize: 20, borderWidth: 2, borderColor: 'white', paddingHorizontal: 20, paddingVertical: 10 }}>TAP HERE TO GO BACK</Text>
-        </TouchableOpacity>
+        <Animatable.View animation="fadeIn" easing="ease-out" iterationCount={1} style={styles.container}>
+          <Image style={{ width: 250, height: 100 }} source={ErrorImage}/>
+          <Text style={{color: 'white', fontSize: 15 }}>Uh Oh! Looks like this picture doesn't work!</Text>
+          <TouchableOpacity onPress={this._returnHome} style={{paddingTop: 50}}>
+            <Text style={{color: 'white', fontSize: 20, borderWidth: 2, borderColor: 'white', paddingHorizontal: 20, paddingVertical: 10 }}>TAP HERE TO GO BACK</Text>
+          </TouchableOpacity>
+        </Animatable.View>
       </View>
     );
   }
