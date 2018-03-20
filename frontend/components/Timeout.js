9f3ad152b8b75fc051802bbb785519b4876e2a89
Shaun Macpherson <shaunmacpherson@Shauns-MacBook-Pro.local>
2018-03-20 15:37:53 -0700

    replaced copy on timeout page

--- frontend/components/Timeout.js
+++ frontend/components/Timeout.js
@@ -14,6 +14,7 @@ import {
 } from 'react-native';
 import Exponent, { Constants, registerRootComponent, LinearGradient } from 'expo';
 import TimeOut from '../assets/timeout.png'
+import TimeOutMessage from '../assets/timeout-message.png'
 import * as Animatable from 'react-native-animatable';
 
 
@@ -35,7 +36,7 @@ export default class Timeout extends React.Component {
         <Animatable.View animation="fadeIn" easing="ease-out" iterationCount={1} style={styles.container}>
           <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
           <Image style={{ width: 280, height: 130 }} source={TimeOut}/>
-          <Text style={{color: 'white', fontSize: 15, textAlign: 'center' }}>Uh Oh! Looks like your connection timed out! {"\n"}{"\n"} Try clearing your phones cache or restarting your phone!</Text>        
+          <Image source={TimeOutMessage}/>
           <TouchableOpacity onPress={this._returnHome} style={{paddingTop: 50}}>
             <Text style={{color: 'white', fontSize: 20, borderWidth: 2, borderColor: 'white', paddingHorizontal: 20, paddingVertical: 10 }}>TAP HERE TO GO BACK</Text>
           </TouchableOpacity>
